b53c04d1e689a322efed29fb3def65e4fa165eb0
yurii-ivanisenko <miles@AGAMEMNON>
2023-03-29 23:37:06 +0300

    test-10

--- terraform/aws/INT/us-east-1/init.tf
+++ terraform/aws/INT/us-east-1/init.tf
@@ -1,2 +1,9 @@
-init
+provider "aws" {
+  profile    = "default"
+  region     = "eu-central-1"
+}
 
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
